6b001d21867a514410ec1dedf80f7a90fd9109cc
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-07-12 17:04:48 +0700

    update

--- src/navigator/MainNavigator.js
+++ src/navigator/MainNavigator.js
@@ -1,63 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { Icon } from 'react-native-elements'
-import HomeScreen from '../screen/HomeScreen'
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Icon} from 'react-native-elements';
+import HomeScreen from '../screen/HomeScreen';
+import AddProductScreen from '../screen/AddProductScreen';
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const DrawerNav = () => {
-    return (
-        <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen
-            name='Home'
-            component={HomeScreen}
-            options={{
-<<<<<<< HEAD
-                title: 'Chinese food',
-                headerStyle: {
-                    backgroundColor: '#A8A196'
-=======
-                title: 'Chineesee Food',
-                headerStyle: {
-                    backgroundColor: '#D3E5C2'
->>>>>>> 2e751d722215c61121a00783ef83d24d46375bd2
-                },
-                headerTitleAlign: 'center',
-                drawerIcon: config =>
-                <Icon
-                    name='home'
-                    type='antdesign'
-                />
-
-            }}/>
-
-        </Drawer.Navigator>
-    )
-}
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Chinese food',
+          headerStyle: {
+            backgroundColor: '#A8A196',
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Drawer.Screen
+        name="AddProductScreen"
+        component={AddProductScreen}
+        options={{
+          title: 'Add food',
+          headerStyle: {
+            backgroundColor: '#A8A196',
+          },
+          headerTitleAlign: 'center',
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 const MainNavigator = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Drawer'>
-                <Stack.Screen
-                    name='Drawer'
-                    component={DrawerNav}
-                    options={{
-                        headerShown: false
-                    }}
-                />
-<<<<<<< HEAD
-            
-=======
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Drawer">
+        <Stack.Screen
+          name="Drawer"
+          component={DrawerNav}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
->>>>>>> 2e751d722215c61121a00783ef83d24d46375bd2
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
-
-export default MainNavigator
-
+export default MainNavigator;
