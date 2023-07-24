049fa5facedf1f18fe1de4e73d249951d93f1d7d
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-07-24 17:52:38 +0700

    update

--- src/navigator/MainNavigator.js
+++ src/navigator/MainNavigator.js
@@ -9,6 +9,7 @@ import AddProductScreen from '../screen/AddProductScreen';
 import ShowProductScreen from '../screen/ShowProductScreen';
 import EditProductScreen from '../screen/EditProductScreen';
 import ImageZoomScreen from '../screen/ImageZoomScreen';
+import SplashScreen from '../screen/SplashScreen';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -45,7 +46,7 @@ const DrawerNav = () => {
 const MainNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Drawer">
+      <Stack.Navigator initialRouteName="SplashScreen">
         <Stack.Screen
           name="Drawer"
           component={DrawerNav}
@@ -74,6 +75,13 @@ const MainNavigator = () => {
         <Stack.Screen
           name="ImageZoom"
           component={ImageZoomScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
           options={{
             headerShown: false,
           }}
--- src/screen/SplashScreen.js
+++ src/screen/SplashScreen.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { View, ImageBackground, Image, StyleSheet } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {View, ImageBackground, Image, StyleSheet} from 'react-native';
 
-const SplashScreen = (props) => {
-    const { navigation } = props
-    useEffect(() => {
-        setTimeout(() => {
-            navigation.replace('Drawer')
-        }, 3000)
-    }, [])
+const SplashScreen = props => {
+  const {navigation} = props;
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Drawer');
+    }, 3000);
+  }, []);
 
-    return (
-        <View style={styles.mainContainer}>
-            <ImageBackground style={styles.imageBackground} source={require('../../assets/images/food_splash.pnh')} >
-            </ImageBackground>
-        </View>
-    )
-}
-
+  return (
+    <View style={styles.mainContainer}>
+      <ImageBackground style={styles.imageBackground}>
+        <Image
+          style={styles.image}
+          source={require('../../assets/images/food_splash.png')}
+        />
+      </ImageBackground>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1
-    }, imageBackground: {
-        width: '100%',
-        height: '100%',
-        alignItems: 'center'
-    }, image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'contain'
-    }
-})
+  mainContainer: {
+    flex: 1,
+  },
+  imageBackground: {
+    width: '100%',
+    height: 500,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
 
-export default SplashScreen
+export default SplashScreen;
